d6c7e63905ad7eb5152c71c6ad521a40308f60fe
fuuukeee3 <semarusika@gmail.com>
2020-08-27 23:32:50 +0900

    Pratt構文解析準備

--- ast/ast.go
+++ ast/ast.go
@@ -1,12 +1,15 @@
 package ast
 
 import (
+	"bytes"
+
 	"github.com/fuuukeee3/monkey_lang/token"
 )
 
 // Node is ASTのインターフェース
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
 // Statement is 文のインターフェース
@@ -36,6 +39,15 @@ func (p *Program) TokenLiteral() string {
 	}
 }
 
+func (p *Program) String() string {
+	var out bytes.Buffer
+
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+	return out.String()
+}
+
 // LetStatement is let文の構造体
 type LetStatement struct {
 	Token token.Token // LET
@@ -49,6 +61,22 @@ func (ls *LetStatement) statementNode() {}
 // TokenLiteral is ノードが関連づけられているトークンのリテラルを返す
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
 
+// String is
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+	out.WriteString(";")
+
+	return out.String()
+}
+
 // Identifier is 識別子の構造体
 type Identifier struct {
 	Token token.Token
@@ -61,6 +89,9 @@ func (i *Identifier) expressionNode() {}
 // TokenLiteral is ノードが関連づけられているトークンのリテラルを返す
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
 
+// String is
+func (i *Identifier) String() string { return i.Value }
+
 // ReturnStatement is return文の構造体
 type ReturnStatement struct {
 	Token       token.Token
@@ -72,3 +103,37 @@ func (rs *ReturnStatement) statementNode() {}
 
 // TokenLiteral is
 func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
+
+// String is
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(rs.TokenLiteral() + " ")
+
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+	out.WriteString(";")
+
+	return out.String()
+}
+
+// ExpressionStatement is 式の構造体
+type ExpressionStatement struct {
+	Token      token.Token
+	Expression Expression
+}
+
+// statementNode is
+func (es *ExpressionStatement) statementNode() {}
+
+// TokenLiteral is
+func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
+
+// String is
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+	return ""
+}
