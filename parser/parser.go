7ca354a7652522ba9da45fc4143fcbf8a00c6a0f
fuuukeee3 <semarusika@gmail.com>
2020-08-30 22:46:21 +0900

    真偽値リテラル

--- parser/parser.go
+++ parser/parser.go
@@ -78,6 +78,9 @@ func New(l *lexer.Lexer) *Parser {
 	p.registerInfix(token.LT, p.parseInfixExpression)
 	p.registerInfix(token.GT, p.parseInfixExpression)
 
+	p.registerPrefix(token.TRUE, p.parseBoolean)
+	p.registerPrefix(token.FALSE, p.parseBoolean)
+
 	// curTokenとpeekTokenにセットするため2回読み込む
 	p.nextToken()
 	p.nextToken()
@@ -267,4 +270,8 @@ func (p *Parser) parseInfixExpression(left ast.Expression) ast.Expression {
 	p.nextToken()
 	expression.Right = p.parseExpression(precedence)
 	return expression
+}
+
+func (p *Parser) parseBoolean() ast.Expression {
+	return &ast.Boolean{Token: p.curToken, Value: p.curTokenIs(token.TRUE)}
 }
