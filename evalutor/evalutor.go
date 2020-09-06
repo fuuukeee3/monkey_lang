60532d41a4b1e36b899182f86509ac94d7b9001c
fuuukeee3 <semarusika@gmail.com>
2020-09-06 21:37:38 +0900

    整数リテラル

---
+++ evalutor/evalutor.go
@@ -0,0 +1,29 @@
+package evalutor
+
+import (
+	"github.com/fuuukeee3/monkey_lang/ast"
+	"github.com/fuuukeee3/monkey_lang/object"
+)
+
+func Eval(node ast.Node) object.Object {
+	switch node := node.(type) {
+	case *ast.Program:
+		return evalStatements(node.Statements)
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+	case *ast.IntegerLiteral:
+		return &object.Integer{Value: node.Value}
+	}
+
+	return nil
+}
+
+func evalStatements(stmts []ast.Statement) object.Object {
+	var result object.Object
+
+	for _, statement := range stmts {
+		result = Eval(statement)
+	}
+
+	return result
+}
